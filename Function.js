db5015be626e2559fc567cecb018c7f83bc6e07c
subhankar <bnk.subhankar@gmail.com>
2025-05-05 21:04:57 +0530

    updated notes upto IIFE

--- Function.js
+++ Function.js
@@ -85,9 +85,49 @@ function handleArray(anyarray){
      return anyarray[1]
 }
 
-console.log(handleArray(Arr));
+// console.log(handleArray(Arr));
+
+//------------------------------------------------------------
 
 
+//this keyword
+const User={
+    username:"Subhankar",
+    price:999,
 
+    welcomeMessage: function(){
+        console.log(`${this.username},Welcome to the website`);
+        console.log(this)
+    }
+}
+// User.welcomeMessage()
+// User.username="Sam"
+// User.welcomeMessage()
+
+//Arrow Function
+
+const addtwo=(num1,num2)=>{
+    return num1+num2
+
+}
+// console.log(addtwo(3,3));
+
+//-------------------------------------------------------
+
+
+//Immediately Invoked Function Expressions(IIFE)
+//->It is Used to remove global scope pollution
+
+(function chai()
+{
+    // console.log(`DB CONNECTED`);
+    
+})();
+
+
+((name)=>{
+    console.log(` DB IS CONNECTED WITH ${name}`);
+    
+})("Subhankar")
 
 
--- Scope.js
+++ Scope.js
@@ -15,11 +15,43 @@ if(true)
     let a=10
     const b=20
     var c=30
-    console.log("Inner:",a);
+    // console.log("Inner:",a);
     
 }
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
+//-----------------------------------------------------
 
+function one (){
+    const username="Subhankar"
+
+    function two (){
+        const website="Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()    
+}
+// one() 
+
+
+//--------------------------------------
+
+
+// console.log(addone(5));
+function addone(num)
+{
+    return num+1
+}
+//----------------------------------------
+
+
+// console.log(addtwo(5));
+const addtwo=function(num){
+    return num+2
+}
+
+//---------------------------------------------------------
